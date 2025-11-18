866fb769f0de6932e930a38fa730792b3efdd2a0
Maxim Zubarev <maxim1zubarev@gmail.com>
2025-11-18 21:15:51 +0300

    change app name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mldz.unsplashapp"
         minSdk = 24
         targetSdk = 35
-        versionCode = 106
-        versionName = "1.0.6"
+        versionCode = 107
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
